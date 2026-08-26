4077a4e7224006da6e3bfc22650ce42cc57a30db
The-Dn6rfl00f <me@dn6rfl00f.com>
2026-08-25 23:57:35 -0700

    Initial Terraform config with HCP cloud block

---
+++ main.tf
@@ -0,0 +1,21 @@
+terraform {
+  required_version = "1.15.9"
+
+  cloud {
+    
+    organization = "The_Dn6rfl00f"
+
+    workspaces {
+      name = "Dn6rfl00fTest"
+    }
+  }
+}
+
+
+resource "random_pet" "test" {
+  length = 2
+}
+
+output "pet_name" {
+  value = random_pet.test.id
+}
